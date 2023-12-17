5406b40f51b019588e31157cb52d7014c393ce62
mariaoliv <77213389+mariaoliv@users.noreply.github.com>
2023-12-17 13:08:21 -0500

    Revert "enemies"
    This reverts commit 6de9fc021f1f265056c5a71828bd225e83146e4b.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
     public GameObject bulletPrefab;
 
-    //[SerializeField] Overworld world;
+    [SerializeField] Overworld world;
 
     Vector2 curPos;
 
@@ -114,7 +114,7 @@ public class PlayerController : MonoBehaviour
         if (health <= 0)
         {
             health = 100;
-            SceneManager.LoadScene("NewOverworld_CJ");
+            SceneManager.LoadScene("Overworld");
         }
 
         if (!isOpen)
@@ -219,7 +219,7 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
-                //world.GainXP(10);
+                world.GainXP(10);
                 Destroy(collision.gameObject, 0.5f);
             }
         }
